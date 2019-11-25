7ebe303f3c6e85d0825abf26309bcea0a8916f25
Class Account <ee106a-abu@c111-7.local>
2019-11-24 18:54:16 -0800

    Detect light blue right on target

--- src/image_segmentation/src/track_target.py
+++ src/image_segmentation/src/track_target.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import rospy
+import sys
 import cv2 as cv
 import numpy as np
 from sensor_msgs.msg import Image
@@ -10,16 +11,41 @@ def callback(msg):
     bridge = CvBridge()
     try:
         img = bridge.imgmsg_to_cv2(msg, "passthrough")
+        # img = img[:, 200:1080, :]
         img_gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
-        all_circles = cv.HoughCircles(img_gray, cv.HOUGH_GRADIENT, 1, 20,
-                            param1=50, param2=30, minRadius=5, maxRadius=30)
+        # img_gray = cv.Canny(img_gray, 100, 200)
+        # cv.imshow('edges', img_gray)
+        # cv.waitKey(1)
+
+        # Threshold on light blue
+        img_hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+        lower_thresh = (78,30,54)
+        upper_thresh = (108,255,255)
+        mask = cv.inRange(img_hsv, lower_thresh, upper_thresh)
+        img_gray = cv.bitwise_and(img_gray, img_gray, mask=mask)
+        img_gray = cv.medianBlur(img_gray, 13)
+        cv.imshow('thresholding', img_gray)
+        cv.waitKey(1)
+
+        # img_gray = cv.Canny(img_gray, 100, 200)
+        # cv.imshow('edges', img_gray)
+        # cv.waitKey(1)
+
+        all_circles = cv.HoughCircles(img_gray, cv.HOUGH_GRADIENT, 2, 0.1,
+                            param1=50, param2=100, minRadius=15, maxRadius=100)
         circle = all_circles[0, 0, :]
         circle = np.uint16(np.around(circle))
         # draw the outer circle
         cv.circle(img, (circle[0], circle[1]), circle[2], (0, 255, 0), 2)
         # draw the center of the circle
         cv.circle(img, (circle[0], circle[1]), 2, (0, 0, 255), 3)
+
+        # for i in all_circles[0,:]:
+        #     # draw the outer circle
+        #     cv.circle(img,(i[0],i[1]),i[2],(0,255,0),2)
+        #     # draw the center of the circle
+        #     cv.circle(img_gray,(i[0],i[1]),2,(0,0,255),3)
         cv.imshow('detected circles', img)
         cv.waitKey(1)
     except CvBridgeError, e:
@@ -27,7 +53,8 @@ def callback(msg):
 
 def main():
     rospy.init_node('camera_sub', anonymous=True)
-    rospy.Subscriber('/cameras/head_camera/image', Image, callback)
+    rospy.Subscriber('/cameras/left_hand_camera/image', Image, callback)
+    # rospy.Subscriber('/io/internal_camera/head_camera/image_raw', Image, callback)
     rospy.spin()
 
 if __name__ == '__main__':
