74dfbda4d09ec59f9205e87c9f5f21042b7bf0b9
Class Account <ee106a-abu@c111-10.local>
2019-12-08 23:56:42 -0800

    Track target using camera projection matrix

--- src/move_gun/src/move_arm.py
+++ src/move_gun/src/move_arm.py
@@ -14,70 +14,68 @@ import traceback
 import shoot
 
 planner = PathPlanner("right_arm")
-
+print("got path planner")
 
 def calc_line(trans):
-	print("moving arm")
-	x_target = trans.transform.translation.x
-	y_target = trans.transform.translation.y
-	z_target = trans.transform.translation.z
-	print("{},{},{}".format(x_target, y_target, z_target))
+    print("moving arm")
+    x_target = trans.transform.translation.x
+    y_target = trans.transform.translation.y
+    z_target = trans.transform.translation.z
+    print("{},{},{}".format(x_target, y_target, z_target))
 
-	while not rospy.is_shutdown():
-	    try:
-	        goal_1 = PoseStamped()
-	        goal_1.header.frame_id = "base"
-	        
-	        # x, y, and z position
-	        goal_1.pose.position.x = 0.5
-	        goal_1.pose.position.y = y_target
-	        goal_1.pose.position.z = z_target
-	        
-	        # Orientation as a quaternion (must be normalized to one)
-	        # q = quaternion_from_euler(-3.14, 0, -1.57)
-	        q = quaternion_from_euler(-3.14, 0, -1.48)
-	        goal_1.pose.orientation.x = q[0]
-	        goal_1.pose.orientation.y = q[1]
-	        goal_1.pose.orientation.z = q[2]
-	        goal_1.pose.orientation.w = q[3]
+    while not rospy.is_shutdown():
+        try:
+            goal_1 = PoseStamped()
+            goal_1.header.frame_id = "base"
+            
+            # x, y, and z position
+            goal_1.pose.position.x = 0.5
+            goal_1.pose.position.y = y_target
+            goal_1.pose.position.z = z_target
+            
+            # Orientation as a quaternion (must be normalized to one)
+            q = quaternion_from_euler(3.100, 0.154, -1.428)
+            goal_1.pose.orientation.x = q[0]
+            goal_1.pose.orientation.y = q[1]
+            goal_1.pose.orientation.z = q[2]
+            goal_1.pose.orientation.w = q[3]
 
-	        # Might have to edit this . . . 
-	        plan = planner.plan_to_pose(goal_1, [])
+            # Might have to edit this . . . 
+            plan = planner.plan_to_pose(goal_1, [])
 
-	        raw_input("Press <Enter> to move the right arm to goal pose 1: ")
-	        if not planner.execute_plan(plan):
-	            raise Exception("Execution failed")
+            raw_input("Press <Enter> to move the right arm to goal pose 1: ")
+            if not planner.execute_plan(plan):
+                raise Exception("Execution failed")
 
-	        raw_input("Press <Enter> to shoot: ")
-	        shoot.shoot()
-	        raw_input("Press <Enter> when done reloading: ")
-	        shoot.hold()
-	    except Exception as e:
-	        print e
-	        traceback.print_exc()
-	    else:
-	        break
+            raw_input("Press <Enter> to shoot: ")
+            shoot.shoot()
+            raw_input("Press <Enter> when done reloading: ")
+            shoot.hold()
+        except Exception as e:
+            print e
+            traceback.print_exc()
+        else:
+            break
 
 def main():
+    rospy.init_node('move_arm')
 
-	rospy.init_node('move_arm')
+    shoot.init()
 
-	shoot.init()
-
-	tfBuffer = tf2_ros.Buffer()
-	tfListener = tf2_ros.TransformListener(tfBuffer)
-	source_frame = "base"
-	target_frame = "target"
-	rate = rospy.Rate(1000.0)
-	while not rospy.is_shutdown():
-		try:
-			trans = tfBuffer.lookup_transform(source_frame, target_frame, rospy.Time())
-			calc_line(trans)
-			print("getting target location")
-		except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException) as e:
-			# print(e)
-			rate.sleep()
-			continue
+    tfBuffer = tf2_ros.Buffer()
+    tfListener = tf2_ros.TransformListener(tfBuffer)
+    source_frame = "base"
+    # target_frame = "target"
+    target_frame = "target_new"
+    rate = rospy.Rate(1000.0)
+    while not rospy.is_shutdown():
+        try:
+            trans = tfBuffer.lookup_transform(source_frame, target_frame, rospy.Time())
+            calc_line(trans)
+            print("getting target location")
+        except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException) as e:
+            rate.sleep()
+            continue
 
 
 if __name__ == '__main__':
--- src/track_target/src/track_target.py
+++ src/track_target/src/track_target.py
@@ -15,8 +15,8 @@ from cv_bridge import CvBridge, CvBridgeError
 LOWER_THRESH = (15,65,50)
 # UPPER_THRESH = (50,255,255)
 UPPER_THRESH = (70,255,200)
-CAMERA_HEIGHT = 800
-CAMERA_WIDTH = 1280
+# CAMERA_HEIGHT = 800
+# CAMERA_WIDTH = 1280
 
 # Diameter (in m) of the circle detected by the algorithm
 CIRCLE_DIAMETER = 0.048
@@ -108,7 +108,7 @@ def calc_target_position(circle, CAMERA_HEIGHT, CAMERA_WIDTH):
     relative_depth = image_height * TARGET_FULL_DIST / TARGET_DIAMETER
     print("relative_depth: {}".format(relative_depth))
 
-    # Calculate how far above/below the target is relative to camera
+    # # Calculate how far above/below the target is relative to camera
     circle_y_frac = circle_y / CAMERA_HEIGHT
     print(circle_y_frac)
     abs_y_pos = image_height * circle_y_frac
@@ -139,17 +139,20 @@ def calc_target_position(circle, CAMERA_HEIGHT, CAMERA_WIDTH):
     t.child_frame_id = "target"
     br.sendTransform(t)
 
-"""
-    base_target_trans = TransformStamped()
-    base_target_trans.header.stamp = rospy.Time.now()
-    base_target_trans.header.frame_id = "base"
-    base_target_trans.child_frame_id = "target"
-    base_target_trans.transform.translation.x = t.transform.translation.x + base_camera_trans.transform.translation.x
-    base_target_trans.transform.translation.y = t.transform.translation.y + base_camera_trans.transform.translation.y
-    base_target_trans.transform.translation.z = t.transform.translation.z + base_camera_trans.transform.translation.z
-    base_target_trans.transform.rotation.w = 1
-    br.sendTransform(base_target_trans)
-"""
+    relative_x_pos = relative_depth * (circle_x - 667.5471339020004) / 412.07883144382936
+    relative_y_pos = relative_depth * (circle_y - 426.61500902127045) / 413.0922741963357
+
+    br = tf2_ros.TransformBroadcaster()
+    t = TransformStamped()
+    t.transform.translation.x = relative_y_pos
+    t.transform.translation.y = -relative_x_pos
+    t.transform.translation.z = relative_depth
+    t.transform.rotation.w = 1
+    t.header.stamp = rospy.Time.now() + rospy.Duration(337)  # Compensating for robot publishing wrong timestamps
+    t.header.frame_id = "left_hand_camera_axis"
+    t.child_frame_id = "target_new"
+    br.sendTransform(t)
+
 
 def callback(msg):
     try:
