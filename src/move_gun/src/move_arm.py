abf81679f40626b6f65f2747a284bbcc48781b60
Class Account <ee106a-abu@c111-10.local>
2019-12-10 00:14:14 -0800

    Continuous tracking and attempt at trajectory prediction

--- src/move_gun/src/move_arm.py
+++ src/move_gun/src/move_arm.py
@@ -12,6 +12,7 @@ from path_planner import PathPlanner
 from baxter_interface import Limb
 import traceback
 import shoot
+from datetime import datetime
 
 planner = PathPlanner("right_arm")
 
@@ -37,41 +38,44 @@ def calc_line(trans):
                 goal_1.pose.position.z = z_target
                 
                 # Orientation as a quaternion (must be normalized to one)
-                q = quaternion_from_euler(2.940, -0.340, -1.65)
+                # q = quaternion_from_euler(2.940, -0.340, -1.65)
+                q = quaternion_from_euler(3.000, -0.144, -1.65)
                 goal_1.pose.orientation.x = q[0]
                 goal_1.pose.orientation.y = q[1]
                 goal_1.pose.orientation.z = q[2]
                 goal_1.pose.orientation.w = q[3]
             else:
                 print("target out of range")
-                goal_1.pose.position.x = 0.5
-                goal_1.pose.position.y = 0.15
-                # goal_1.pose.position.z = 0
-                goal_1.pose.position.z = z_target
+            #     goal_1.pose.position.x = 0.5
+            #     goal_1.pose.position.y = 0.15
+            #     # goal_1.pose.position.z = 0
+            #     goal_1.pose.position.z = z_target
                 
-                # Orientation as a quaternion (must be normalized to one)
-                theta_yaw = -1.65 + np.arctan((y_target - goal_1.pose.position.y) / (x_target - goal_1.pose.position.x))
-                # theta_roll = 2.940 + np.arctan((z_'target - goal_1.pose.position.z) / (x_target - goal_1.pose.position.x))
-                # if theta_roll > np.pi:
-                #     theta_roll -= 2 * np.pi
-                theta_roll = 2.940
-                q = quaternion_from_euler(theta_roll, -0.340, theta_yaw)
-                goal_1.pose.orientation.x = q[0]
-                goal_1.pose.orientation.y = q[1]
-                goal_1.pose.orientation.z = q[2]
-                goal_1.pose.orientation.w = q[3]
+            #     # Orientation as a quaternion (must be normalized to one)
+            #     theta_yaw = -1.65 + np.arctan((y_target - goal_1.pose.position.y) / (x_target - goal_1.pose.position.x))
+            #     # theta_roll = 2.940 + np.arctan((z_'target - goal_1.pose.position.z) / (x_target - goal_1.pose.position.x))
+            #     # if theta_roll > np.pi:
+            #     #     theta_roll -= 2 * np.pi
+            #     theta_roll = 2.940
+            #     q = quaternion_from_euler(theta_roll, -0.340, theta_yaw)
+            #     goal_1.pose.orientation.x = q[0]
+            #     goal_1.pose.orientation.y = q[1]
+            #     goal_1.pose.orientation.z = q[2]
+            #     goal_1.pose.orientation.w = q[3]
 
             # Might have to edit this . . . 
             plan = planner.plan_to_pose(goal_1, [])
+            if len(plan.joint_trajectory.points) > 0:
+                print(plan.joint_trajectory.points[-1].time_from_start.to_sec())
 
-            raw_input("Press <Enter> to move the right arm to goal pose 1: ")
+            # raw_input("Press <Enter> to move the right arm to goal pose 1: ")
             if not planner.execute_plan(plan):
                 raise Exception("Execution failed")
 
-            raw_input("Press <Enter> to shoot: ")
-            shoot.shoot()
-            raw_input("Press <Enter> when done reloading: ")
-            shoot.hold()
+            # raw_input("Press <Enter> to shoot: ")
+            # shoot.shoot()
+            # raw_input("Press <Enter> when done reloading: ")
+            # shoot.hold()
         except Exception as e:
             print e
             traceback.print_exc()
@@ -90,20 +94,21 @@ def main():
     source_frame = "base"
     # target_frame = "target"
     target_frame = "target_new"
-    rate = rospy.Rate(1000.0)
+    rate = rospy.Rate(1.0)
     while not rospy.is_shutdown():
         try:
-            # trans = tfBuffer.lookup_transform(source_frame, target_frame, rospy.Time())
-            # calc_line(trans)
-
             trans_list = []
             for _ in range(10):
-                trans = tfBuffer.lookup_transform(source_frame, target_frame, rospy.Time())
+                trans = tfBuffer.lookup_transform(source_frame, target_frame, rospy.Time(0))
+                # print(trans.header.stamp)
+                print(datetime.utcfromtimestamp(trans.header.stamp.secs).strftime('%M:%S'))
                 trans_list.append(trans.transform.translation)
             x = np.median([i.x for i in trans_list])
             y = np.median([i.y for i in trans_list])
             z = np.median([i.z for i in trans_list])
             calc_line((x, y, z))
+            print("----------------------")
+            rate.sleep()
             
         except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException) as e:
             rate.sleep()
