4149a7f81aa1fefb5425552bf23dd754a3d86bcd
shreyas1230 <shreyas1230@hotmail.com>
2019-12-12 01:22:35 -0800

    hom filter not working

--- src/track_target/src/temp.py
+++ src/track_target/src/temp.py
@@ -110,100 +110,206 @@ from matplotlib import pyplot as plt
 # circle = detect_target_circle(img_output)
 
 
-import cv2
-import argparse
-from operator import xor
+# import cv2
+# import argparse
+# from operator import xor
+#
+# def callback(arg):
+#     pass
+#
+# def threshold(range_filter, frame_to_thresh):
+#     v1_min, v2_min, v3_min, v1_max, v2_max, v3_max = get_trackbar_values(range_filter)
+#
+#     thresh = cv2.inRange(frame_to_thresh, (v1_min, v2_min, v3_min), (v1_max, v2_max, v3_max))
+#     thresh = cv2.medianBlur(thresh, 9)
+#
+#     cv2.imshow("Original", cv2.cvtColor(frame_to_thresh, cv2.COLOR_HSV2BGR))
+#     cv2.imshow("thresholded image", thresh)
+#     # cv2.imshow("Thresh", thresh)
+#     # cv2.waitKey(1)
+#
+#     if cv2.waitKey(1) & 0xFF is ord('q'):
+#         return
+#
+#
+# def setup_trackbars(range_filter):
+#     cv2.namedWindow("Trackbars", 0)
+#
+#     for i in ["MIN", "MAX"]:
+#         v = 0 if i == "MIN" else 255
+#
+#         for j in range_filter:
+#             cv2.createTrackbar("%s_%s" % (j, i), "Trackbars", v, 255, callback)
+#
+#
+# def get_arguments():
+#     ap = argparse.ArgumentParser()
+#     ap.add_argument('-f', '--filter', required=True,
+#                     help='Range filter. RGB or HSV')
+#     ap.add_argument('-p', '--preview', required=False,
+#                     help='Show a preview of the image after applying the mask',
+#                     action='store_true')
+#     args = vars(ap.parse_args())
+#     if not args['filter'].upper() in ['RGB', 'HSV']:
+#         ap.error("Please speciy a correct filter.")
+#
+#     return args
+#
+#
+# def get_trackbar_values(range_filter):
+#     values = []
+#
+#     for i in ["MIN", "MAX"]:
+#         for j in range_filter:
+#             v = cv2.getTrackbarPos("%s_%s" % (j, i), "Trackbars")
+#             values.append(v)
+#
+#     return values
+#
+#
+# def main():
+#     args = get_arguments()
+#
+#     range_filter = args['filter'].upper()
+#
+#     image = cv2.imread('../img/archytas_left_camera_2.png')
+#
+#
+#
+#     img_yuv = cv2.cvtColor(image, cv2.COLOR_BGR2YUV)
+#
+#     # equalize the histogram of the Y channel
+#     # img_yuv[:,:,0] = cv2.equalizeHist(img_yuv[:,:,0])
+#     clahe = cv.createCLAHE(clipLimit=2.0, tileGridSize=(16,16))
+#     img_yuv[:,:,0] = clahe.apply(img_yuv[:,:,0])
+#     # convert the YUV image back to RGB format
+#     image = cv2.cvtColor(img_yuv, cv2.COLOR_YUV2BGR)
+#
+#
+#
+#     if range_filter == 'RGB':
+#         frame_to_thresh = image.copy()
+#     else:
+#         frame_to_thresh = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+#     # else:
+#     #     camera = cv2.VideoCapture(0)
+#
+#     setup_trackbars(range_filter)
+#
+#     while True:
+#         threshold(range_filter, frame_to_thresh)
+#
+#
+# if __name__ == '__main__':
+#     main()
 
-def callback(arg):
-    pass
-
-def threshold(range_filter, frame_to_thresh):
-    v1_min, v2_min, v3_min, v1_max, v2_max, v3_max = get_trackbar_values(range_filter)
-
-    thresh = cv2.inRange(frame_to_thresh, (v1_min, v2_min, v3_min), (v1_max, v2_max, v3_max))
-    thresh = cv2.medianBlur(thresh, 9)
-
-    cv2.imshow("Original", cv2.cvtColor(frame_to_thresh, cv2.COLOR_HSV2BGR))
-    cv2.imshow("thresholded image", thresh)
-    # cv2.imshow("Thresh", thresh)
-    # cv2.waitKey(1)
-
-    if cv2.waitKey(1) & 0xFF is ord('q'):
-        return
-
-
-def setup_trackbars(range_filter):
-    cv2.namedWindow("Trackbars", 0)
-
-    for i in ["MIN", "MAX"]:
-        v = 0 if i == "MIN" else 255
-
-        for j in range_filter:
-            cv2.createTrackbar("%s_%s" % (j, i), "Trackbars", v, 255, callback)
-
-
-def get_arguments():
-    ap = argparse.ArgumentParser()
-    ap.add_argument('-f', '--filter', required=True,
-                    help='Range filter. RGB or HSV')
-    ap.add_argument('-p', '--preview', required=False,
-                    help='Show a preview of the image after applying the mask',
-                    action='store_true')
-    args = vars(ap.parse_args())
-    if not args['filter'].upper() in ['RGB', 'HSV']:
-        ap.error("Please speciy a correct filter.")
-
-    return args
-
-
-def get_trackbar_values(range_filter):
-    values = []
-
-    for i in ["MIN", "MAX"]:
-        for j in range_filter:
-            v = cv2.getTrackbarPos("%s_%s" % (j, i), "Trackbars")
-            values.append(v)
-
-    return values
-
-
-def main():
-    args = get_arguments()
-
-    range_filter = args['filter'].upper()
-
-    image = cv2.imread('../img/archytas_left_camera_2.png')
 
 
 
-    img_yuv = cv2.cvtColor(image, cv2.COLOR_BGR2YUV)
-
-    # equalize the histogram of the Y channel
-    # img_yuv[:,:,0] = cv2.equalizeHist(img_yuv[:,:,0])
-    clahe = cv.createCLAHE(clipLimit=2.0, tileGridSize=(16,16))
-    img_yuv[:,:,0] = clahe.apply(img_yuv[:,:,0])
-    # convert the YUV image back to RGB format
-    image = cv2.cvtColor(img_yuv, cv2.COLOR_YUV2BGR)
-
-
-
-    if range_filter == 'RGB':
-        frame_to_thresh = image.copy()
-    else:
-        frame_to_thresh = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-    # else:
-    #     camera = cv2.VideoCapture(0)
-
-    setup_trackbars(range_filter)
-
-    while True:
-        threshold(range_filter, frame_to_thresh)
-
-
-if __name__ == '__main__':
-    main()
-
-
+# # Homomorphic filter class
+# class HomomorphicFilter:
+#     """Homomorphic filter implemented with diferents filters and an option to an external filter.
+#
+#     High-frequency filters implemented:
+#         butterworth
+#         gaussian
+#     Attributes:
+#         a, b: Floats used on emphasis filter:
+#             H = a + b*H
+#
+#         .
+#     """
+#
+#     def __init__(self, a = 0.5, b = 1.5):
+#         self.a = float(a)
+#         self.b = float(b)
+#
+#     # Filters
+#     def __butterworth_filter(self, I_shape, filter_params):
+#         P = I_shape[0]/2
+#         Q = I_shape[1]/2
+#         U, V = np.meshgrid(range(I_shape[0]), range(I_shape[1]), sparse=False, indexing='ij')
+#         Duv = (((U-P)**2+(V-Q)**2)).astype(float)
+#         H = 1/(1+(Duv/filter_params[0]**2)**filter_params[1])
+#         return (1 - H)
+#
+#     def __gaussian_filter(self, I_shape, filter_params):
+#         P = I_shape[0]/2
+#         Q = I_shape[1]/2
+#         H = np.zeros(I_shape)
+#         U, V = np.meshgrid(range(I_shape[0]), range(I_shape[1]), sparse=False, indexing='ij')
+#         Duv = (((U-P)**2+(V-Q)**2)).astype(float)
+#         H = np.exp((-Duv/(2*(filter_params[0])**2)))
+#         return (1 - H)
+#
+#     # Methods
+#     def __apply_filter(self, I, H):
+#         H = np.fft.fftshift(H)
+#         I_filtered = (self.a + self.b*H)*I
+#         return I_filtered
+#
+#     def filter(self, I, filter_params, filter='butterworth', H = None):
+#         """
+#         Method to apply homormophic filter on an image
+#         Attributes:
+#             I: Single channel image
+#             filter_params: Parameters to be used on filters:
+#                 butterworth:
+#                     filter_params[0]: Cutoff frequency
+#                     filter_params[1]: Order of filter
+#                 gaussian:
+#                     filter_params[0]: Cutoff frequency
+#             filter: Choose of the filter, options:
+#                 butterworth
+#                 gaussian
+#                 external
+#             H: Used to pass external filter
+#         """
+#
+#         #  Validating image
+#         if len(I.shape) is not 2:
+#             raise Exception('Improper image')
+#
+#         # Take the image to log domain and then to frequency domain
+#         I_log = np.log1p(np.array(I, dtype="float"))
+#         I_fft = np.fft.fft2(I_log)
+#
+#         # Filters
+#         if filter=='butterworth':
+#             H = self.__butterworth_filter(I_shape = I_fft.shape, filter_params = filter_params)
+#         elif filter=='gaussian':
+#             H = self.__gaussian_filter(I_shape = I_fft.shape, filter_params = filter_params)
+#         elif filter=='external':
+#             print('external')
+#             if len(H.shape) is not 2:
+#                 raise Exception('Invalid external filter')
+#         else:
+#             raise Exception('Selected filter not implemented')
+#
+#         # Apply filter on frequency domain then take the image back to spatial domain
+#         I_fft_filt = self.__apply_filter(I = I_fft, H = H)
+#         I_filt = np.fft.ifft2(I_fft_filt)
+#         I = np.exp(np.real(I_filt))-1
+#         return np.uint8(I)
+# # End of class HomomorphicFilter
+#
+# if __name__ == "__main__":
+#     import cv2
+#     # Code parameters
+#     img_path = '../img/archytas_left_camera_5.png'
+#
+#
+#     # Main code
+#     img = cv2.imread(img_path)
+#     img = cv2.cvtColor(img, cv.COLOR_BGR2GRAY)
+#     # img_yuv = cv2.cvtColor(img, cv.COLOR_BGR2YUV)
+#     # img_y = img_yuv[:, :, 0]
+#     homo_filter = HomomorphicFilter(a = 0.75, b = 1.25)
+#     img_filtered = homo_filter.filter(I=img, filter_params=[30,2])
+#     # img_yuv[:,:,0] = img_filtered
+#     cv.imshow('original', img)
+#     cv.imshow('filtered', img_filtered)
+#     cv.waitKey(0)
 
 
 # img = cv.imread('/home/eecs106a/ros_workspaces/EE106A-AimBot/src/track_target/img/archytas_left_camera_5.png')
